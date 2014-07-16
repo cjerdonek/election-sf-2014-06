44b271d48f10fdcc06fac42a585656365c835abb
Chris Jerdonek <chris.jerdonek@gmail.com>
2014-07-16 05:25:37 -0700

    Finish initial version of script and add output.

--- analyze.py
+++ analyze.py
@@ -3,6 +3,8 @@ Usage: python analyze.py
 
 The above needs to be run from the root of the source repository.
 
+The script writes its output to a file called out.csv.
+
 """
 
 # Terminology notes:
@@ -22,13 +24,30 @@ import sys
 CHALLENGED_VBM_PATH = "data/Challenged_VBMs_June_2014.csv"
 # Has the precinct ID and name.
 PRECINCT_NEIGHBORHOOD_PATH = "data/Precinct-Neighborhoods_20140321.csv"
+# Uses precinct ID.
+PROVISIONAL_REPORT_PATH = "data/Provisional Ballot Report_June 2014.csv"
 # Uses the precinct name.
 SOV_PATH = "data/SOV_JUN2014_Governor.csv"
 
 KEYS = [
-    'counted_in_person',
-    'counted_vbm',
-    'challenged_vbm',
+    'in_person_counted',
+    'vbm_counted',
+    'vbm_uncounted',
+    'provisional_cast',
+    'provisional_uncounted',  # equivalent to "in_person_uncounted"
+]
+
+ALL_KEYS = [
+    'all_cast',  # in_person_counted + vbm_counted + provisional_uncounted + vbm_uncounted
+    'in_person_cast',  # in_person_counted + provisional_uncounted
+    'vbm_cast',  # vbm_counted + vbm_uncounted
+    'provisional_cast',
+    'total_counted',  # in_person_counted + vbm_counted
+    'in_person_counted',
+    'vbm_counted',
+    'total_uncounted',  # vbm_uncounted + provisional_uncounted
+    'vbm_uncounted',
+    'provisional_uncounted',
 ]
 
 # This string maps neighborhood labels in the precinct-to-neighborhood
@@ -130,7 +149,7 @@ def process_sov(precinct_name_to_id, precinct_totals):
             except KeyError:
                 raise KeyError("%r not in: %r" % (precinct_id, precinct_ids))
             data = precinct_totals[precinct_id]
-            data['counted_in_person'] += int(row[8])
+            data['in_person_counted'] += int(row[8])
 
             # 2) Process VBM precinct total.
             # For example: "Pct 1101 - VBM Reporting"
@@ -140,7 +159,7 @@ def process_sov(precinct_name_to_id, precinct_totals):
                 raise AssertionError(repr(parts))
             if parts[0][4:] != precinct_name:
                 raise AssertionError(repr(parts))
-            data['counted_vbm'] += int(row[8])
+            data['vbm_counted'] += int(row[8])
     # Make sure all precincts were listed in the SOV file.
     assert not len(precinct_ids)
 
@@ -153,13 +172,52 @@ def process_challenged_vbm(precinct_totals):
         # Skip the header row.
         reader.next()
         for row in reader:
-            precinct_id, challenged = row[0], row[10]
+            precinct_id, challenged = row[0], int(row[10])
             try:
                 precinct_ids.remove(precinct_id)
             except KeyError:
                 raise KeyError("%r not in: %r" % (precinct_id, precinct_ids))
             data = precinct_totals[precinct_id]
-            data['challenged_vbm'] += int(challenged)
+            data['vbm_uncounted'] += challenged
+
+def process_provisionals(precinct_totals):
+    """Add challenged VBM data to the given precinct totals."""
+    precinct_ids = set(precinct_totals.keys())
+
+    with open(PROVISIONAL_REPORT_PATH) as f:
+        reader = csv.reader(f)
+        # Skip the header row.
+        reader.next()
+        for row in reader:
+            precinct, uncounted, cast = row[0], int(row[-3]), int(row[-1])
+            precinct_id = precinct.split()[1]
+            try:
+                precinct_ids.remove(precinct_id)
+            except KeyError:
+                print "precinct ID not found: %r" % precinct_id
+                if cast:
+                    raise KeyError("%r not in: %r" % (precinct_id, precinct_ids))
+                else:
+                    # Skip the precinct if there were no totals.
+                    # The provisional report seems to have extra empty
+                    # precincts, e.g. precinct 9998.
+                    continue
+            data = precinct_totals[precinct_id]
+            data['provisional_cast'] += int(cast)
+            data['provisional_uncounted'] += int(uncounted)
+    # Make sure all precincts were listed in the file.
+    assert not len(precinct_ids)
+
+def compute_extra_fields(precinct_totals):
+    for data in precinct_totals.values():
+        def subtotal(*keys):
+            return sum(data[k] for k in keys)
+        data['all_cast'] = subtotal('in_person_counted', 'vbm_counted',
+                                    'provisional_uncounted', 'vbm_uncounted')
+        data['in_person_cast'] = subtotal('in_person_counted', 'provisional_uncounted')
+        data['vbm_cast'] = subtotal('vbm_counted', 'vbm_uncounted')
+        data['total_counted'] = subtotal('in_person_counted', 'vbm_counted')
+        data['total_uncounted'] = subtotal('vbm_uncounted', 'provisional_uncounted')
 
 def make_nbhd_totals(precinct_to_nbhd, p_totals):
     """
@@ -170,37 +228,54 @@ def make_nbhd_totals(precinct_to_nbhd, p_totals):
     # Initialize the neighborhood totals to zero.
     n_totals = {}
     for name in nbhd_key.values():
-        n_totals[name] = dict.fromkeys(KEYS, 0)
+        n_totals[name] = dict.fromkeys(ALL_KEYS, 0)
 
     for precinct_id, p_data in p_totals.iteritems():
         nbhd_label = precinct_to_nbhd[precinct_id]
         nbhd_name = nbhd_key[nbhd_label]
         n_data = n_totals[nbhd_name]
-        for k in KEYS:
+        for k in ALL_KEYS:
             n_data[k] += p_data[k]
 
     return n_totals
 
 def write_nbhd_totals(f, nbhd_totals):
     w = csv.writer(f)
-    w.writerow(['precinct'] + KEYS)
+    w.writerow(['Neighborhood'] + ALL_KEYS)
+
     for nbhd in sorted(nbhd_totals.keys()):
         data = nbhd_totals[nbhd]
-        w.writerow([nbhd] + [data[k] for k in KEYS])
+        w.writerow([nbhd] + [data[k] for k in ALL_KEYS])
+
+    # Add a totals row.
+    totals = []
+    for k in ALL_KEYS:
+        total = sum(data[k] for data in nbhd_totals.values())
+        totals.append(total)
+    w.writerow(['TOTAL'] + totals)
 
 def main():
     precinct_to_nbhd, precinct_name_to_id = parse_precinct_nbhd_file()
     precinct_ids = set(precinct_to_nbhd.keys())
     precinct_totals = initialize_precinct_totals(precinct_ids)
 
-    # Calculate total in-person and absentees per precinct.
+    # Now parse precinct totals.
+
+    # In-person and VBMs counted per precinct.
     process_sov(precinct_name_to_id, precinct_totals)
 
+    # VBMs challenged (i.e. not counted) per precinct.
     process_challenged_vbm(precinct_totals)
 
-    # Aggregate precinct totals by neighborhood.
-    n_totals = make_nbhd_totals(precinct_to_nbhd, precinct_totals)
+    # Provisionals (both counted and uncounted) per precinct.
+    process_provisionals(precinct_totals)
 
-    write_nbhd_totals(sys.stdout, n_totals)
+    compute_extra_fields(precinct_totals)
+
+    # Aggregate precinct totals by neighborhood.
+    nbhd_totals = make_nbhd_totals(precinct_to_nbhd, precinct_totals)
+
+    with open('out.csv', 'wb') as f:
+        write_nbhd_totals(f, nbhd_totals)
 
 main()
